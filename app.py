064cf4e2aae3cb98ad691478ef260c40340e9367
JPLnwZa007 <6531501185@lamduan.mfu.ac.th>
2025-05-10 21:13:00 +0700

    app.py

--- app.py
+++ app.py
@@ -11,14 +11,15 @@ st.title("Customer Segmentation App")
 
 # Input section
 st.header("🔍 Predict Segment")
-income = st.number_input("Income", min_value=0)
-
+income = st.number_input("Income", 0 , 1000000)
+kids = st.slider("Number of Kids", 0, 3)
+teens = st.slider("Number of Teens", 0, 3)
 recency = st.number_input("Recency", min_value=0)
 wines = st.number_input("Monthly Wine Spend")
 fruits = st.number_input("Monthly Fruit Spend")
 
 if st.button("Predict Segment"):
-    data = [[income, recency, wines, fruits]]
+    data = [[income, kids, teens, recency, wines, fruits]]
     data_scaled = scaler.transform(data)
     segment = model.predict(data_scaled)
     st.success(f"Predicted Customer Segment: {segment[0]}")
