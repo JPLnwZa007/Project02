a4c94b0b86fd6844dc332a0b640e6b5cfb22680d
JPLnwZa007 <6531501185@lamduan.mfu.ac.th>
2025-05-10 19:09:40 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 18 23:15:47 2025
+
+@author: Nongnuch
+"""
+
+import streamlit as st
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+
+# Load model and scaler
+model = pickle.load(open('kmeans_model.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
+
+st.title("Customer Segmentation App")
+
+# Input section
+st.header("🔍 Predict Segment")
+income = st.number_input("Income", min_value=0)
+kids = st.slider("Number of Kids", 0, 3)
+teens = st.slider("Number of Teens", 0, 3)
+recency = st.number_input("Recency", min_value=0)
+wines = st.number_input("Monthly Wine Spend")
+fruits = st.number_input("Monthly Fruit Spend")
+
+if st.button("Predict Segment"):
+    data = [[income, kids, teens, recency, wines, fruits]]
+    data_scaled = scaler.transform(data)
+    segment = model.predict(data_scaled)
+    st.success(f"Predicted Customer Segment: {segment[0]}")
+
+# Visualization section
+st.header("📊 Customer Segment Distribution")
+
+try:
+    df = pd.read_csv("segmented_customers.csv")
+
+    fig, ax = plt.subplots()
+    df['Segment'].value_counts().sort_index().plot(kind='bar', ax=ax)
+    ax.set_xlabel("Segment")
+    ax.set_ylabel("Number of Customers")
+    ax.set_title("Customer Count by Segment")
+    st.pyplot(fig)
+
+except FileNotFoundError:
+    st.warning("segmented_customers.csv not found. Please run the training script first.")
